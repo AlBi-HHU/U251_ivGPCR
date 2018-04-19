26ffb3e78a89e8369962627e861df1f8647bb201
Gunnar W. Klau <gunnar.klau@hhu.de>
2018-04-19 13:32:16 +0200

    - multiple scores (FC, score) 0 extra categories (fixed pathways)

--- scripts/proteins.py
+++ scripts/proteins.py
@@ -7,8 +7,9 @@ import os
 if not os.path.exists("eXamine"):
     os.makedirs("eXamine")
 
-parser = argparse.ArgumentParser(description = 'Transfers heinz output to eXamineS annotations')
+parser = argparse.ArgumentParser(description = 'Transfers heinz output to eXamine annotations')
 parser.add_argument('-m', type = str, help = 'name of file containing module nodes .res', required = True)
+parser.add_argument('-fc', type = str, help = 'name of file containing fold change values', required = True)
 parser.add_argument('-ol', type = str, help = 'links file name', required = True)
 parser.add_argument('-on', type = str, help = 'nodes file name', required = True)
 parser.add_argument('-om', type = str, help = 'modules annotation file name', required = True)
@@ -25,24 +26,37 @@ with open(args.m, 'rt') as heinzfile:
     for row in reader:
         proteins[row['#label']] = [row['score']]
 
+#print(proteins)
+
+with open(args.fc, 'rt') as foldchangefile:
+    reader = csv.DictReader(foldchangefile, delimiter='\t')#, fieldnames = ("Id", "log2FoldChange", "padj"), header=0)
+    for row in reader:
+        proteins[row['']].append(row['log2FoldChange'])
+        #proteins[row['']].append(row['padj'])
+        #print(proteins[row['Id']])#.append(row['log2FoldChanage'])
+
+#print(proteins)
+
 with open(args.ol, 'wt') as links_output:
     writer = csv.writer(links_output, delimiter='\t')
     for protein in proteins.keys():
         writer.writerow(['small', protein])
 
 with open(args.on, 'wt') as protein_output:
-    header = ['Identifier', 'Score', 'Symbol', 'URL']
+    header = ['Identifier', 'FC : Number', 'Score : Number', 'Symbol', 'URL : Href']
     writer = csv.DictWriter(protein_output, delimiter = '\t', fieldnames = header)
     writer.writeheader()
     for protein, annotation in proteins.items():
+        #print(protein, annotation)
         entry = {}
         entry['Identifier'] = protein
-        entry['Score'] = annotation[0]
-        if len(annotation) == 2:
-            entry['Symbol'] = annotation[1]
+        entry['FC : Number'] = annotation[1]
+        entry['Score : Number'] = annotation[0]
+        if len(annotation) == 3:
+            entry['Symbol'] = annotation[3]
         else:
             entry['Symbol'] = protein
-        entry['URL'] = 'http://www.genecards.org/cgi-bin/carddisp.pl?gene='+ protein
+        entry['URL : Href'] = 'http://www.genecards.org/cgi-bin/carddisp.pl?gene='+ protein
         writer.writerow(entry)
 
 with open(args.om, 'wt') as annotations:
--- scripts/run_heinz.py
+++ scripts/run_heinz.py
@@ -20,28 +20,34 @@ a = params.readline().strip('\n\r')
 FDR = snakemake.wildcards.FDR
 experiment = snakemake.wildcards.experiment
 network = snakemake.wildcards.network
+
 fullnetwork = 'networks/' + snakemake.config['networks'][network]
 
-subprocess_heinz = ["heinz",
-                    "-a", a,
-                    "-lambda", l,
-                    "-FDR", FDR,
-                    "-n", snakemake.input.pvals,
-                    "-e", fullnetwork,
-                    #"-e", "networks/HPRD_Release9_062910_Heinz.txt",
-                    #"-e", "networks/9606.protein.links.v10_mapped_geneIDs_700_no_UBC.txt",
-                    "-o", "scores/{}_{}_{}_module.txt".format(experiment, network, FDR),
-                    #"-r", "TP53",
-                    "-v", '0']
+for i in range(1):
 
-                    # ">", "scores/{}_{}_module.dot".format(experiment, FDR)]
+    subprocess_heinz = ["heinz",
+                        "-a", a,
+                        "-lambda", l,
+                        "-FDR", FDR,
+                        "-n", snakemake.input.pvals,
+                        "-e", fullnetwork,
+                        #"-e", "networks/HPRD_Release9_062910_Heinz.txt",
+                        #"-e", "networks/9606.protein.links.v10_mapped_geneIDs_700_no_UBC.txt",
+                        "-o", "scores/{}_{}_{}_module.txt".format(experiment, network, FDR),
+                        #"-o", "scores/{}_{}_{}_module_{}.txt".format(experiment, network, FDR, i),
+                        #"-r", "TP53",
+                        "-v", '0']
+                        # ">", "scores/{}_{}_module.dot".format(experiment, FDR)]
 
-logger.info('Running Heinz on {} with FDR {} and network {}...'.format(experiment, FDR, network))
-#logger.info('Running Heinz on {} with FDR={}...'.format(experiment, FDR))
-#print(" ".join(subprocess_heinz))
-
-
-dotfile = open("scores/{}_{}_{}_module.dot".format(experiment, network, FDR), 'w')
-p = subprocess.run(subprocess_heinz, stdout=dotfile)
-subprocess.call("grep -v NaN scores/{}_{}_{}_module.txt | grep -v \"#\" > scores/{}_{}_{}_module.mod".format(experiment, network, FDR, experiment, network, FDR), shell=True)
-subprocess.call("neato -Tpdf scores/{}_{}_{}_module.dot -o scores/{}_{}_{}_module.pdf".format(experiment, network, FDR, experiment, network, FDR), shell=True)
+    logger.info('Running Heinz on {} with FDR {} and network {}, computing module {} ...'.format(experiment, FDR, network, i))
+    #logger.info('Running Heinz on {} with FDR={}...'.format(experiment, FDR))
+    #print(" ".join(subprocess_heinz))
+    #dotfile = open("scores/{}_{}_{}_module_{}.dot".format(experiment, network, FDR, i), 'w')
+    dotfile = open("scores/{}_{}_{}_module.dot".format(experiment, network, FDR), 'w')
+    #errfile = open("/tmp/heinz.err", 'w')
+    #print(" ".join(subprocess_heinz))
+    p = subprocess.run(subprocess_heinz, stdout=dotfile)
+    #subprocess.call("grep -v NaN scores/{}_{}_{}_module_{}.txt | grep -v \"#\" > scores/{}_{}_{}_module_{}.mod".format(experiment, network, FDR, i, experiment, network, FDR, i), shell=True)
+    subprocess.call("grep -v NaN scores/{}_{}_{}_module.txt | grep -v \"#\" > scores/{}_{}_{}_module.mod".format(experiment, network, FDR, experiment, network, FDR), shell=True)
+    #subprocess.call("neato -Tpdf scores/{}_{}_{}_module_{}.dot -o scores/{}_{}_{}_module_{}.pdf".format(experiment, network, FDR, i, experiment, network, FDR, i), shell=True)
+    subprocess.call("neato -Tpdf scores/{}_{}_{}_module.dot -o scores/{}_{}_{}_module.pdf".format(experiment, network, FDR, experiment, network, FDR), shell=True)
